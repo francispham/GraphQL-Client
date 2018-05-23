bbe89f1fa0f7a5f74ca4b520d17762be628ee2e9
Francis <francispham89@gmail.com>
2018-05-22 22:23:41 -0700

    Setup w MongoDB

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://phongp:Heroman1989@ds051843.mlab.com:51843/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
