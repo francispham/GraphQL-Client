605ee815e2d3ab606ff6c1400d45773fc3b65741
Francis <francispham89@gmail.com>
2018-05-24 11:49:43 -0700

    Created nav for mutation, fetching & queries directory

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -11,17 +13,19 @@ class SongCreate extends Component {
 
   onSubmit(event) {
     event.preventDefault();
-    console.log(this.props)
+
     this.props.mutate({
       variables: {
         title: this.state.title
-      }
-    });
+      },
+      refetchQueries: [{ query }]
+    }).then(() => hashHistory.push('/'));
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
