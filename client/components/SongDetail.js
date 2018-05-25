4e61609fa5f51a80bd568692c7c2bec716b9f7fb
Francis <francispham89@gmail.com>
2018-05-24 19:43:41 -0700

    Created LyricCreate

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
+import LyricCreate from './LyricCreate';
+
 class SongDetail extends Component {
   render() {
-    console.log(this.props)
+
+    const { song } = this.props.data;
+
+    if(!song) { return <div>Loading...</div> }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
+        <LyricCreate />
       </div>
     );
   }
